77bb29ac07f0ded5ad66ce77a309b794f9b9234b
AlaBouali <trap.leader.123@gmail.com>
2021-01-16 11:39:40 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ if (sys.platform == "win32") or( sys.platform == "win64"):
 
 setuptools.setup(
     name="bane",
-    version="4.3.8",
+    version="4.3.9",
     author="AlaBouali",
     author_email="trap.leader.123@gmail.com",
     description="cyber security library",
